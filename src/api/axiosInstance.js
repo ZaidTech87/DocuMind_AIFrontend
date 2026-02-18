3bc0158edc0159c7651b911e23d8099ee6862a2a
Mohd Zaid <zaidalig1234@gmail.com>
2026-02-18 23:32:39 +0530

    heieheeeeeeeeee

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,32 +1,32 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-});
+// const api = axios.create({
+//     baseURL: import.meta.env.VITE_API_URL
+// });
 
-// Inject JWT
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
+// // Inject JWT
+// api.interceptors.request.use((config) => {
+//     const token = localStorage.getItem('token');
 
-    if (token) {
-        config.headers = config.headers || {};
-        config.headers.Authorization = `Bearer ${token}`;
-    }
+//     if (token) {
+//         config.headers = config.headers || {};
+//         config.headers.Authorization = `Bearer ${token}`;
+//     }
 
-    return config;
-});
+//     return config;
+// });
 
-// Handle expired token
-api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response?.status === 401) {
-            localStorage.removeItem('token');
-            window.location.href = '/login';
-        }
+// // Handle expired token
+// api.interceptors.response.use(
+//     (response) => response,
+//     (error) => {
+//         if (error.response?.status === 401) {
+//             localStorage.removeItem('token');
+//             window.location.href = '/login';
+//         }
 
-        return Promise.reject(error);
-    }
-);
+//         return Promise.reject(error);
+//     }
+// );
 
-export default api;
+// export default api;
